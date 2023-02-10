79b9a08faf711507d53ad43f19929ae29d7264e9
rajupalumaru <raju.palumaru94@gmail.com>
2023-02-10 20:08:32 +0530

    Added ImageSlider

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import ImgSlider from '../Slider/ImgSlider'
 
 const Home = () => {
   return (
-    <div>
-      I am in home Page
-    </div>
+    <Container>
+      <ImgSlider/>
+    </Container>
   )
 }
-
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    position:relative;
+    padding: 0 calc(3.5vw + 5px);
+    overflow-x: hidden;
+    &:before {
+      background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;        
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: -1;
+  }
+ 
+ `
 export default Home
